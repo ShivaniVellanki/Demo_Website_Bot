061a597df3f26443915e55ec5c04dd0a145e5242
ShivaniVellanki <139008609+ShivaniVellanki@users.noreply.github.com>
2026-02-17 19:20:20 -0500

    Add chat configuration for KoreChatSDK

---
+++ chatConfig.js
@@ -0,0 +1,23 @@
+// Wait for analytics to be available
+document.addEventListener('DOMContentLoaded', function() {
+    console.log('Analytics object before getting:', window.analytics);
+    let insideData = getAnalytics();
+    console.log('Analytics data retrieved:', insideData);
+    
+var chatConfig = KoreChatSDK.chatConfig;
+var chatWindow = KoreChatSDK.chatWindow;
+let botOptions=chatConfig.botOptions;
+
+userIdentity = crypto.randomUUID();
+
+botOptions.JWTUrl = "https://epson-jwt-multi.vercel.app/api/sts?bot=na-customer";
+ botOptions.userIdentity = userIdentity;
+ botOptions.botInfo = { name: "Prod Epson Test", "_id": "st-59523fcb-9dd0-589b-a29f-93655dc3bac3" };
+ botOptions.clientId = "cs-59734efa-4252-516f-a92a-60bb5d16c73d";
+
+ chatConfig.history.enable = false;
+var chatWindowInstance = new chatWindow(chatConfig);
+
+chatWindowInstance.show(chatConfig);
+    new KoreChatSDK.chatWindow().show(KoreChatSDK.chatConfig);
+});
